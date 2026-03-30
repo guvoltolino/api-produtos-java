4bfed99ccb01ef7aa5e302f476958a3d60fbb87a
Gustavo Voltolino <gustavo.voltolino@outlook.com>
2026-03-29 22:30:15 -0300

    Implement Service, controller and Main

--- src/main/java/org/example/controller/ProductController.java
+++ src/main/java/org/example/controller/ProductController.java
@@ -1,4 +1,29 @@
 package org.example.controller;
 
+import org.example.model.Product;
+import org.example.service.ProductService;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/products")
 public class ProductController {
+
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping
+    public List<Product> getAllProducts() {
+        return productService.getAllProducts();
+    }
+
+    @PostMapping
+    public Product createProduct(@RequestBody Product product) {
+        return productService.createProduct(product);
+    }
 }
+
--- src/main/java/org/example/service/ProductService.java
+++ src/main/java/org/example/service/ProductService.java
@@ -1,4 +1,24 @@
 package org.example.service;
+import org.example.model.Product;
+import org.springframework.stereotype.Service;
+import java.util.ArrayList;
+import java.util.List;
 
+@Service
 public class ProductService {
+    private List<Product> products = new ArrayList<>();
+
+    public ProductService() {
+        products.add(new Product(1, "Produto A", "Descrição do Produto A", 10)),
+        products.add(new Product(2, "Produto B", "Descrição do Produto B", 20));
+    }
+
+    public List<Product> getAllProducts() {
+        return products;
+    }
+
+    public Product createProduct(Product product) {
+        products.add(product);
+        return product;
+    }
 }
